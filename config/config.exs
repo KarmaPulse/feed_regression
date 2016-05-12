49adec8534ebf836c398a30c86627a730f345714
Jovanny Cruz <jovannypcg@yahoo.com>
2016-05-11 19:04:37 -0500

    feat(app): Remove unneccessary Heroku configuration

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :maru, FeedRegression.API, http: [port: { :system, "8880" }]
+config :maru, FeedRegression.API, http: [port: 8880]
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
